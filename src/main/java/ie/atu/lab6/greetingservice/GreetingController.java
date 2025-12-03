5c052d63919294693260dfa5d71cedfeac9cb30e
JAMESDONNELLAN-STUDE <G00434671@atu.ie>
2025-12-03 14:52:02 +0000

    Added Greeting Controller

---
+++ src/main/java/ie/atu/lab6/greetingservice/GreetingController.java
@@ -0,0 +1,22 @@
+package ie.atu.lab6.greetingservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController
+{
+
+    @GetMapping("/health")
+    public String health()
+    {
+        return "ok";
+    }
+
+    @GetMapping("/api/greet/{name}")
+    public String greet(@PathVariable String name)
+    {
+        return "Hello " + name + " From Service A!";
+    }
+}
